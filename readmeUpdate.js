62be66d09ce5a2d3e1f388b3132b1a621214728d
HYH0804 <guswnsgur2276@gmail.com>
2026-08-13 00:01:57 +0900

    main.yml  cron 1시간으로 변경

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -9,9 +9,77 @@ const staticContent = `
 
 ---
 
-## 이 페이지는 Tistory에 끄적이는 일상 및 일지를 Github를 통해 기록한 흔적입니다.
+## 🚀 About Me
+
+- 🧐 Backend 개발을 기반으로 Full Stack Developer를 목표로 하고 있습니다.
+- 🌱 현재 React를 집중적으로 공부하고 있습니다.
+- 🧑‍💻 새로운 기술을 배우고 직접 만들어보는 것을 좋아합니다.
+- 🎯 취업 준비 및 소프트웨어마에스트로(18기) 준비를 병행하고 있습니다.
+
 ---
+
+## 🎥 소백 (YouTube)
+
+> 소프트웨어 마에스트로 18기 준비 및 취업 준비를 병행하며, 개발 공부와 삽질 과정을 기록하는 공간입니다.
+
+[![YouTube Channel](https://img.shields.io/badge/YouTube-소백%20채널%20바로가기-FF0000?style=for-the-badge&logo=youtube&logoColor=white)](https://www.youtube.com/@somaHomeProtector)
+
+---
+
+## 🌐 Connect with Me
+
+<a href="mailto:guswnsgur2276@gmail.com"><img src="https://img.shields.io/badge/Gmail-EA4335?style=for-the-badge&logo=gmail&logoColor=white"/></a>
+<a href="https://guswnsgur2276.tistory.com/"><img src="https://img.shields.io/badge/Blog-000000?style=for-the-badge&logo=tistory&logoColor=white"/></a>
+
+---
+
+## 🛠️ Technologies & Tools
+
+### 💻 Backend
+<p>
+  <img src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white"/>
+  <img src="https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white"/>
+  <img src="https://img.shields.io/badge/spring%20boot-%236DB33F.svg?style=for-the-badge&logo=springboot&logoColor=white"/>
+  <img src="https://img.shields.io/badge/jpa-%2359666C.svg?style=for-the-badge&logo=hibernate&logoColor=white"/>
+  <img src="https://img.shields.io/badge/mybatis-%23000000.svg?style=for-the-badge&logo=mybatis&logoColor=white"/>
+</p>
+
+### 🌐 Frontend
+<p>
+  <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"/>
+  <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"/>
+  <img src="https://img.shields.io/badge/javascript-%23F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black"/>
+  <img src="https://img.shields.io/badge/react-%2361DAFB.svg?style=for-the-badge&logo=react&logoColor=black"/>
+  <img src="https://img.shields.io/badge/Redux%20Toolkit-%23764ABC.svg?style=for-the-badge&logo=redux&logoColor=white"/>
+  <img src="https://img.shields.io/badge/recoil-%233578E5.svg?style=for-the-badge&logo=recoil&logoColor=white"/>
+  <img src="https://img.shields.io/badge/eslint-%234B32C3.svg?style=for-the-badge&logo=eslint&logoColor=white"/>
+  <img src="https://img.shields.io/badge/antd-%230170FE.svg?style=for-the-badge&logo=antdesign&logoColor=white"/>
+  <img src="https://img.shields.io/badge/styled--components-%23DB7093.svg?style=for-the-badge&logo=styled-components&logoColor=white"/>
+  <img src="https://img.shields.io/badge/react--router-%23CA4245.svg?style=for-the-badge&logo=reactrouter&logoColor=white"/>
+  <img src="https://img.shields.io/badge/immer-%2300E7C0.svg?style=for-the-badge&logo=immer&logoColor=black"/>
+  <img src="https://img.shields.io/badge/SWR-%23000000.svg?style=for-the-badge&logo=swr&logoColor=white"/>
+</p>
+
+### 🚀 Infrastructure & Tools
+<p>
+  <img src="https://img.shields.io/badge/aws-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white"/>
+  <img src="https://img.shields.io/badge/docker-%232496ED.svg?style=for-the-badge&logo=docker&logoColor=white"/>
+  <img src="https://img.shields.io/badge/nginx-%23009639.svg?style=for-the-badge&logo=nginx&logoColor=white"/>
+  <img src="https://img.shields.io/badge/github%20actions-%232088FF.svg?style=for-the-badge&logo=github-actions&logoColor=white"/>
+</p>
+
+---
+
 ## 📕 Latest Blog Posts
+`;
+
+const staticContentAfter = `
+
+---
+## 📍 Profile Views
+
+![Views](https://visitor-badge.laobi.icu/badge?page_id=HYH0804)
+
 `;
 
 // ----- [2] 자동 갱신 영역: 블로그 RSS 읽어서 최신 글 목록 추가 -----
@@ -40,7 +108,7 @@ const parser = new Parser({
   }
 
   // ----- [3] 파일 작성 -----
-  const finalContent = staticContent + blogSection;
+  const finalContent = staticContent + blogSection + staticContentAfter;
   writeFileSync("README.md", finalContent, "utf8");
 
   console.log("README 업데이트 완료");
